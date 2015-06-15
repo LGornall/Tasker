497b62fd704c5f121f1e89098a843c48755a11a9
LGornall <gornall2112@gmail.com>
2015-06-15 19:36:50 +0100

    creating new addTask view... more options

---
+++ Tasker/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Tasker
+//
+//  Created by Louis Gornall on 20/05/2015.
+//  Copyright (c) 2015 Louis Gornall. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tasker/TaskTypes.swift
@@ -0,0 +1,16 @@
+//
+//  TaskTypes.swift
+//  Tasker
+//
+//  Created by Louis Gornall on 06/06/2015.
+//  Copyright (c) 2015 Louis Gornall. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class TaskTypes: NSManagedObject {
+
+    @NSManaged var title: String
+
+}
